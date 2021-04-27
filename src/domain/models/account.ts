138859c2779d308d6c691f4e44fb1d82557e9825
Gustavo Santos <gustavosisa@outlook.com>
2021-04-27 17:53:53 -0300

    feat: add account domain

---
+++ src/domain/models/account.ts
@@ -0,0 +1,9 @@
+export interface AccountModel {
+  name: string;
+  email: string;
+  password: string;
+  id: string;
+  role: string;
+  isActive: boolean;
+  age: number;
+}
---
+++ src/domain/usecases/edit-account.ts
@@ -0,0 +1,17 @@
+import { AccountModel } from '../models/account';
+
+export interface EditAccountModel {
+  email?: string;
+  name?: string;
+  role?: string;
+  password?: string;
+  passwordConfirmation?: string;
+  age?: number;
+}
+
+export interface EditAccount {
+  edit(
+    accountId: string,
+    editAccountModel: EditAccountModel,
+  ): Promise<AccountModel>;
+}
