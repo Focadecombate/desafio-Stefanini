ee9fd110992c8fe9807745e8a29500cbb4381054
Gustavo Santos <gustavosisa@outlook.com>
2021-04-27 18:06:54 -0300

    feat(validation layer): add validation layer

---
+++ src/validation/validators/compare-field/compare-field-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../../presentation/errors';
+import { Validation } from '../../../presentation/protocols/validation';
+
+export class CompareFieldValidation implements Validation {
+  private readonly fieldName: string;
+  private readonly fieldToCompareName: string;
+  constructor(fieldName: string, fieldToCompareName: string) {
+    this.fieldName = fieldName;
+    this.fieldToCompareName = fieldToCompareName;
+  }
+
+  validate(input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName);
+    }
+  }
+}
---
+++ src/validation/validators/email-validation/email-validation.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from '../../../presentation/errors';
+import { Validation } from '../../../presentation/protocols/validation';
+import { EmailValidator } from '../../protocols/email-validator';
+
+export class EmailValidation implements Validation {
+  private readonly emailValidator: EmailValidator;
+  private readonly fieldName: string;
+  constructor(fieldName: string, emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+    this.fieldName = fieldName;
+  }
+
+  validate(input: any): Error {
+    const isValid = this.emailValidator.isValid(input[this.fieldName]);
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName);
+    }
+  }
+}
