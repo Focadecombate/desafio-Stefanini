9244f86645c6b40129db962cd25561cd8dedbcd7
Gustavo Santos <gustavosisa@outlook.com>
2021-04-27 17:25:15 -0300

    chore(project): start project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'standard'
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    es2021: true
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    'no-useless-constructor': 'off',
+    'no-return-assign': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
+}
